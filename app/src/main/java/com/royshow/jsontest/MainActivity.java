758d19bd680d0fb3742d7206770a5ae2defc15e2
royshow0316 <royshow0316@gmail.com>
2016-11-19 15:51:00 +0800

    init

---
+++ app/src/main/java/com/royshow/jsontest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.royshow.jsontest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private OkHttpClient client = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Request request = new Request.Builder().url("你的Json Url").build();
+        Call call = client.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                Log.d("OkHttp", json);
+            }
+        });
+    }
+}
